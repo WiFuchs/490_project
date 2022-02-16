502c76be6e3c6e12ef390c46114eb4cfcca9f318
wfuchs <wfuchs@calpoly.edu>
2022-02-16 13:01:33 -0800

    add visitorCombine scaffolding

---
+++ visitorCombine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Will Fuchs on 2/16/22.
+//
+
+#include "visitorCombine.h"
---
+++ visitorCombine.h
@@ -0,0 +1,27 @@
+//
+// Created by Will Fuchs on 2/16/22.
+//
+
+#ifndef INC_490_PROJECT_VISITORCOMBINE_H
+#define INC_490_PROJECT_VISITORCOMBINE_H
+
+#include <map>
+#include "Visitor.h"
+#include "demogCombo.h"
+#include "psCombo.h"
+using namespace std;
+
+class visitorCombine : public Visitor{
+public:
+    shared_ptr<demogCombo> getComboDemogData(string regionName) {return comboDemog.find(regionName)->second;}
+    shared_ptr<psCombo> getComboPoliceData(string regionName)  {return comboPolice.find(regionName)->second;}
+    std::map<string, shared_ptr<demogCombo> >& getComboDemog() {return comboDemog;}
+    std::map<string, shared_ptr<psCombo> > & getComboPolice() {return comboPolice;}
+
+private:
+    std::map<string, shared_ptr<psCombo> > comboPolice;
+    std::map<string, shared_ptr<demogCombo> > comboDemog;
+};
+
+
+#endif//INC_490_PROJECT_VISITORCOMBINE_H
